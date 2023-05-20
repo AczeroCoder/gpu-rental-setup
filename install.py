9dba5683f3c7354c36f082b007f0dfdc52c017a2
Bilal <103607205+AczeroCoder@users.noreply.github.com>
2023-05-20 01:22:28 +0100

    Update install.py

--- install.py
+++ install.py
@@ -7,31 +7,35 @@ def get_machine_id():
     with open('/etc/machine-id') as f:
         return f.read().strip()
 
-api_key = getpass.getpass('Please enter your API key: ')
-gpu_details = {}  # Assuming this is the format you want
-gpu_details['gpu_model'] = input('Enter your GPU Model: ')
-gpu_details['vram'] = input('Enter your VRAM: ')
-gpu_details['rent_price'] = input('Enter the rent price: ')
-
 # Send the details to your server
-response = requests.post('https://3f07-2a02-c7e-2818-f900-5076-55ee-5fc7-a963.ngrok-free.app/add_gpu', headers={'X-Api-Key': api_key}, json=gpu_details)
+getUID = get_machine_id()
+response1 = requests.post('https://3f07-2a02-c7e-2818-f900-5076-55ee-5fc7-a963.ngrok-free.app/get_api_key', headers={'getKey': getUID})
 
-if response.status_code == 200:
-    print('GPU details submitted successfully.')
-    # If API key is valid, save it for future requests
-    with open('~/.myapp/api_key_file', 'w') as f:
-        f.write(api_key)
+if response1.status_code == 200:
+    print(response1.text)
+    api_key = response1.text
+    gpu_details = {}
+    gpu_details['gpu_model'] = input('Enter your GPU Model: ')
+    gpu_details['vram'] = input('Enter your VRAM: ')
+    gpu_details['rent_price'] = input('Enter the rent price: ')
 
-    # Download the daemon
-    subprocess.run(["wget", "https://github.com/AczeroCoder/gpu-rental-setup/blob/main/daemon.py"])
+    response = requests.post('https://3f07-2a02-c7e-2818-f900-5076-55ee-5fc7-a963.ngrok-free.app/add_gpu', headers={'X-Api-Key': api_key}, json=gpu_details)
 
-    # Make the daemon file executable
-    subprocess.run(["chmod", "+x", "daemon.py"])
+    if response.status_code == 200:
+        print('GPU details submitted successfully.')
 
-    # Schedule the daemon to run every 30 seconds
-    crontab_line = "*/30 * * * * /usr/bin/python3 ~/.myapp/daemon.py"
-    with open("/etc/crontab", "a") as cron_file:
-        cron_file.write(crontab_line)
+        # Download the daemon
+        subprocess.run(["wget", "https://github.com/AczeroCoder/gpu-rental-setup/blob/main/daemon.py"])
 
+        # Make the daemon file executable
+        subprocess.run(["chmod", "+x", "daemon.py"])
+
+        # Schedule the daemon to run every 30 seconds
+        crontab_line = "*/30 * * * * /usr/bin/python3 ~/.myapp/daemon.py"
+        with open("/etc/crontab", "a") as cron_file:
+            cron_file.write(crontab_line)
+
+    else:
+        print(f'Error: {response.status_code}. {response.text}')
 else:
-    print(f'Error: {response.status_code}. {response.text}')
+    print(f'Error: {response1.status_code}. {response1.text}')
