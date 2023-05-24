4e87524c6f1ef3418bc9f8fb45a5c8052ff3ceae
Bilal <103607205+AczeroCoder@users.noreply.github.com>
2023-05-24 02:06:22 +0100

    Update gpureq.py

--- gpureq.py
+++ gpureq.py
@@ -4,9 +4,10 @@ import json
 import os
 
 api_key = "3412423422" # test api key
+source = "https://9fd4-90-211-254-60.ngrok-free.app"
 
 # Ping the server
-response = requests.post('https://3f07-2a02-c7e-2818-f900-5076-55ee-5fc7-a963.ngrok-free.app/daemon_check', headers={'X-Api-Key': api_key})
+response = requests.post(source + '/daemon_check', headers={'X-Api-Key': api_key})
 
 # If the response contains instructions
 if response.status_code == 200:
